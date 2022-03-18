bebab23ff10d93f944c991f8ace407e5c330bc41
GGergibt <gosha2007naz@gmail.com>
2022-03-18 23:37:38 +0300

    dump newest database

--- crontab_RMdir.py
+++ crontab_RMdir.py
@@ -1,23 +1,39 @@
 import os
 import shutil
-x = os.listdir("/home/www/code/django_server/django_ggrksok/media/youtube")
-p = os.listdir("/home/www/code/django_server/django_ggrksok/media/tiktok")
 # x = os.listdir("/home/www/code/python_server/media")
-for i in x:
-    # shutil.rmtree(f"/home/www/code/django_server/django_ggrksok/media/{i}")
-    os.remove(f"/home/www/code/django_server/django_ggrksok/media/youtube/{i}")
-for i in p:
-    os.remove(f"/home/www/code/django_server/django_ggrksok/media/tiktok/{i}")
-# u = os.listdir(f"/home/www/code/django_server/django_ggrksok/instagram")
-g = os.listdir(f"/home/www/code/django_server/django_ggrksok/media/tt_profile")
-for i in g:
-    shutil.rmtree(f"/home/www/code/django_server/django_ggrksok/media/tt_profile/{i}")
-u = os.listdir(f"/home/www/code/python_server/instagram")
-for y in u:
-    shutil.rmtree(f"/home/www/code/python_server/instagram/{y}")
-    # shutil.rmtree(f"/home/www/code/django_server/django_ggrksok/instagram/{y}")
-u = os.listdir(f"/home/www/code/python_server/highlights")
-# u = os.listdir(f"/home/www/code/django_server/django_ggrksok/highlights")
-for y in u:
-    shutil.rmtree(f"/home/www/code/python_server/highlights/{y}")
-    # shutil.rmtree(f"/home/www/code/django_server/django_ggrksok/highlights/{y}")
+try:
+    x = os.listdir("/home/www/code/django_server/django_ggrksok/media/youtube")
+    for i in x:
+        # shutil.rmtree(f"/home/www/code/django_server/django_ggrksok/media/{i}")
+        os.remove(f"/home/www/code/django_server/django_ggrksok/media/youtube/{i}")
+except FileNotFoundError:
+    print("youtube already clean")
+try:
+    p = os.listdir("/home/www/code/django_server/django_ggrksok/media/tiktok")
+    for i in p:
+        os.remove(f"/home/www/code/django_server/django_ggrksok/media/tiktok/{i}")
+except FileNotFoundError:
+    print("tiktok already clean")
+    # u = os.listdir(f"/home/www/code/django_server/django_ggrksok/instagram")
+try:
+    g = os.listdir(f"/home/www/code/django_server/django_ggrksok/media/tt_profile")
+    for i in g:
+        shutil.rmtree(f"/home/www/code/django_server/django_ggrksok/media/tt_profile/{i}")
+except FileNotFoundError:
+    print("tt_profiles already clean")
+try:
+    u = os.listdir("/home/www/code/python_server2/instagram")
+    for y in u:
+        shutil.rmtree(f"/home/www/code/python_server2/instagram/{y}")
+        # shutil.rmtree(f"/home/www/code/django_server/django_ggrksok/instagram/{y}")
+except FileNotFoundError:
+    print("instagram stories already clean")
+try:
+    # u = os.listdir(f"/home/www/code/django_server/django_ggrksok/highlights")
+    u = os.listdir("/home/www/code/python_server2/highlights")
+    for y in u:
+        shutil.rmtree(f"/home/www/code/python_server2/highlights/{y}")
+        # shutil.rmtree(f"/home/www/code/django_server/django_ggrksok/highlights/{y}")
+except FileNotFoundError:
+
+        print("highlights already clean")
--- database_requests.py
+++ database_requests.py
@@ -10,7 +10,7 @@ connection_to_database = psycopg2.connect(
 )
 connection_to_database.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cursor = connection_to_database.cursor()
-def insert_account_in_database_if_not_exists(cursor, chat_id: int, account: str, profiles_column: str)-> bool:
+def insert_account_in_database_if_not_exists(chat_id: int, account: str, profiles_column: str)-> bool:
     """insert in user_table user''s chat_id if not exists and insta/tiktok profiles if not exists. If exists return False, else return True. Account : profile which need to save, profiles_colum : name of column where account need to save, can be insta/tt accounts. Chat_id : user''s ID. download_count : count user''s download """
     sql_selecting = f"SELECT MAX(download_count) FROM user_table WHERE chat_id = {chat_id} AND {profiles_column} = '{account}'"
     execute_command = cursor.execute(sql_selecting)
@@ -29,14 +29,14 @@ def insert_account_in_database_if_not_exists(cursor, chat_id: int, account: str,
         inserting_command = cursor.execute(sql_inserting)
         return True
     
-def selecting_accounts_if_exists(cursor, chat_id: int, profiles_column: str):
+def selecting_accounts_if_exists(chat_id: int, profiles_column: str):
     """Return user''s save profiles if exists, else return False"""
     sql_selecting = f"SELECT {profiles_column} FROM user_table WHERE chat_id = {chat_id} and {profiles_column} != 'null' GROUP BY {profiles_column} ORDER BY MAX(download_count) DESC LIMIT 3"
     execute_command = cursor.execute(sql_selecting)
     write_answer = cursor.fetchall()
     return write_answer
 
-def selecting_download_count_if_exists(cursor, chat_id: int, profiles_column):
+def selecting_download_count_if_exists(chat_id: int, profiles_column):
     sql_selecting = f"SELECT MAX(download_count) FROM user_table WHERE chat_id = {chat_id} and {profiles_column} != 'null' GROUP BY {profiles_column} ORDER BY MAX(download_count) DESC LIMIT 3"
     # if not write_answer:
     execute_command = cursor.execute(sql_selecting)
